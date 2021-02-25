b0e79dc8452d061a147e1bc043770ab836a7300d
Endika <endikatapia954@gmail.com>
2021-02-25 15:39:14 +0100

    Nuevo Toast

--- app/src/main/java/com/example/pruebagit/MainActivity.java
+++ app/src/main/java/com/example/pruebagit/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pruebagit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"Hola mundo",Toast.LENGTH_LONG);
     }
 }
